5dece9129ef6ed76d215b148de0d2c52355995a0
Fred <frederico.d.silva@outlook.com>
2020-05-16 17:47:09 -0300

    adicionando entrada de arquivo

--- src/main/java/com/br/controller/ProcessaCorridaController.java
+++ src/main/java/com/br/controller/ProcessaCorridaController.java
@@ -1,27 +1,27 @@
 package com.br.controller;
 
-import java.io.File;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.br.service.ProcessaCorridaService;
 
 @Controller
 @RestController
-@RequestMapping(value = "/processar-corrida")
+@RequestMapping(value = "/processa-corrida")
 public class ProcessaCorridaController {
 
 	@Autowired
 	private ProcessaCorridaService service;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> processarCorridda(File file) {
-		service.processarCorrida(file);
+	public ResponseEntity<Void> processarCorridda(@RequestParam("file") MultipartFile file) {
+		System.out.println("oi");
 		return null;
 
 	}
