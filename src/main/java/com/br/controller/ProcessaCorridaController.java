c832bdf54b18aa21d07c88967f8d70dbe359b742
Fred <frederico.d.silva@outlook.com>
2020-05-16 18:03:44 -0300

    salvando e lendo o arquivo

--- src/main/java/com/br/controller/ProcessaCorridaController.java
+++ src/main/java/com/br/controller/ProcessaCorridaController.java
@@ -1,5 +1,12 @@
 package com.br.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,9 +27,22 @@ public class ProcessaCorridaController {
 	private ProcessaCorridaService service;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> processarCorridda(@RequestParam("file") MultipartFile file) {
-		System.out.println("oi");
-		return null;
+	public ResponseEntity<Void> processarCorridda(@RequestParam("file") MultipartFile file) throws IOException {
 
+		if (file != null) {
+			File copied = new File("./temp/file.csv");
+			InputStream input = file.getInputStream();
+			OutputStream out = new BufferedOutputStream(new FileOutputStream(copied));
+
+			byte[] buffer = new byte[1024];
+			int lengthRead;
+			while ((lengthRead = input.read(buffer)) > 0) {
+				out.write(buffer, 0, lengthRead);
+				out.flush();
+			}
+		}
+
+		service.processarCorrida();
+		return null;
 	}
 }
--- src/main/java/com/br/service/ProcessaCorridaService.java
+++ src/main/java/com/br/service/ProcessaCorridaService.java
@@ -1,13 +1,40 @@
 package com.br.service;
 
-import java.io.File;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProcessaCorridaService {
 
-	public void processarCorrida(File file) {
-		System.out.println("oi");
+	public void processarCorrida() {
+		String arquivoCSV = "./temp/file.csv";
+	    BufferedReader br = null;
+	    String linha = "";
+    
+		try {
+
+	        br = new BufferedReader(new FileReader(arquivoCSV));
+	        while ((linha = br.readLine()) != null) {
+
+	            System.out.println(linha);
+	        }
+
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (br != null) {
+	            try {
+	                br.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+		
 	}
 }
