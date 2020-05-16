7757b6347feecee1747dffc76aefffab6144656d
Fred <frederico.d.silva@outlook.com>
2020-05-16 17:26:40 -0300

    configurando projeto

--- src/main/java/com/br/controller/ProcessarCorridaController.java
+++ src/main/java/com/br/controller/ProcessarCorridaController.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.br.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectJavaApplication {
+public class ProcessarCorridaController {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectJavaApplication.class, args);
+		SpringApplication.run(ProcessarCorridaController.class, args);
 	}
 
 }
---
+++ src/main/java/com/br/service/ProcessarCorridaService.java
@@ -0,0 +1,13 @@
+package com.br.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessarCorridaService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessarCorridaService.class, args);
+	}
+
+}
